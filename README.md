3ec4648715b7abfbeb527a51c07899171e02aa09
Jocomol <joelmeier08@gmail.com>
2021-07-24 14:38:24 +0200

    added the possibility to download all videos from one creator only

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ I sure love the mix of newsboat+mpv+youtube-dl to watch videos from my favourite
 
 ## Usage
 Just execute the script and the videos will be downloaded a directory called "Youtube" in your current directory and sorted after the creator of the video.
+
+```
+usage: newsboat_video_downloader [-h] [-c CREATOR [CREATOR ...]] [-t]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CREATOR [CREATOR ...], --creator CREATOR [CREATOR ...]
+                        Download only the videos by a specific creator
+  -t, --testing         Enables the testing mode: not setting items as read after downloading
+```
--- newsboat_video_downloader.py
+++ newsboat_video_downloader.py
@@ -3,6 +3,7 @@
 import sqlite3
 import os
 import errno
+import argparse
 
 
 def make_sure_path_exists(path):
@@ -13,17 +14,24 @@ def make_sure_path_exists(path):
             raise
 
 
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('-c', '--creator', nargs='+', help='Download only the videos by a specific creator', type=str, default="", action="store")
+parser.add_argument('-t', '--testing', help='Enables the testing mode: not setting items as read after downloading', action="store_true")
+args = parser.parse_args()
+
 con = sqlite3.connect(f"{os.environ['HOME']}/.newsboat/cache.db")
 cur = con.cursor()
 
 make_sure_path_exists('./Youtube/')
+creator = " ".join(args.creator)
 
-for row in cur.execute('select url, author from rss_item WHERE "unread"=1 and "url" like "%youtube%";'):
+for row in cur.execute(f'select url, author from rss_item WHERE "unread"=1 and author like "%{creator}%" collate nocase and "url" like "%youtube%";'):
     url, author = row
     folder = author.replace(" ", "_").lower()
     make_sure_path_exists(f'./Youtube/{folder}')
     os.system(f'youtube-dl --restrict-filenames -o "./Youtube/{folder}/%(title)s-%(id)s.%(ext)s" {url}')
 
-cur.execute('update rss_item set "unread" = 0 WHERE "unread"=1 and "url" like "%youtube%";')
+if not args.testing:
+    cur.execute(f'update rss_item set "unread" = 0 WHERE "unread"=1 and author like "%{creator}%" and "url" like "%youtube%";')
 con.commit()
 con.close()
